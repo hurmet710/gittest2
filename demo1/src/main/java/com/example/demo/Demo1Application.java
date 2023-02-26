1bd339cddb1a19439f2a98a92900055e10ff6ebb
hurmet tursnhan <hurmet710@gmail.com>
2023-02-26 19:37:36 +0600

    commit2

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
-        
+
+        System.out.println("我加了一行代码");
+
         SpringApplication.run(Demo1Application.class, args);
         
         
