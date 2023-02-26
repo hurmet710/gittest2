2328396d31515d8bc2dbb74e51f9a231506f8fd3
hurmet tursnhan <hurmet710@gmail.com>
2023-02-26 19:43:15 +0600

    commit2

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -9,6 +9,9 @@ public class Demo1Application {
     public static void main(String[] args) {
 
         System.out.println("我加了一行代码");
+        System.out.println("我加了一行代码");
+        System.out.println("我加了一行代码");
+
 
         SpringApplication.run(Demo1Application.class, args);
         
